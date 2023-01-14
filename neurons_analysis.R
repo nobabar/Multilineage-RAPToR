646a5479a8a7cb70adcb1a2f30ba8bb4ccd5eb43
babar <bapt09.rousseau@gmail.com>
2023-01-14 15:52:04 +0100

    reworked process to add train and test sets

--- neurons_analysis.R
+++ neurons_analysis.R
@@ -14,7 +14,9 @@ library(tradeSeq)
 library(viridis)
 library(wormRef, quietly = T) # for gene IDs, www.github.com/LBMC/wormRef
 
+source("ale.R")
 source("multige_im.R")
+source("utils.R")
 
 # Loading data ----
 
@@ -225,109 +227,58 @@ lins <- list(
   ASE = cell_subtypes %in% ctypes[c(1, 2, 3)]
 )
 
-tpX <- scale(t(pX), scale = FALSE, center = TRUE) 
-
-mt <- multige_im(X = pX, p = p_fpx, lineages = lins,
-                 dim_red = 'ica', nc = 8,
-                 formulas = c("X~s(embryo.time, bs='cr', k=6)",
-                              "X~s(embryo.time, bs='cr', k=6)",
-                              "X~s(embryo.time, bs='cs', k=6)",
-                              "X~s(embryo.time, bs='cr', k=5)",
-                              "X~s(embryo.time, bs='cr', k=5)",
-                              "X~s(embryo.time, bs='cr', k=5)",
-                              "X~s(embryo.time, bs='cr', k=4)",
-                              "X~s(embryo.time, bs='cr', k=4)"))
-
-# predict new data (in comp. space)
-ndat <- data.frame(embryo.time=seq(min(p_fpx$embryo.time), max(p_fpx$embryo.time), l=100))
-nX <- predict_mgeim(mt, ndat, as.c = T)
-interpGE <- predict_mgeim(mt, ndat)
-
-ncs <- 1:8
-sapply(seq_along(mt$lineages), function(li){
-  l <- mt$lineages[[li]]
-  svg(paste0("fig/neurons_lineage_", names(mt$lineages)[li],"_ica.svg"))
-  par(mfrow = c(5,8/2), mar=c(3,1,2,1), bty='l', pty='s')
-  sapply(ncs, function(ci){
-    plot(p_fpx$embryo.time[l], mt$dim_red$S[l,ci], xlab = "time", ylab="IC",
-         col = transp(cols, a=.2), pch = pchs,
-         main = paste0(names(mt$lineages)[li], "lin, IC", mt$dim_red$ncs[ci]),
-         ylim = range(mt$dim_red$S[,ci]), xlim = range(p_fpx$embryo.time))
-    points(p_fpx$embryo.time[-l], mt$dim_red$S[-l,ci], cex = .1, col = cols[l], pch = pchs[l], lw=2)
-    points(ndat$embryo.time, nX[[li]][,ci], type  ='l', col = 'red')
-  })
-  dev.off()
-})
-
-# staging
-
-ae_s <- lapply(interpGE, function(rdat){
-  RAPToR::ae(samp = pX, refdata = rdat, ref.time_series = ndat$embryo.time,
-     bootstrap.n = 30)
-})
-
-svg("fig/neurons_staging.svg")
-par(mfrow = c(3,3), mar=c(3,1,2,1), pty='s', bty='l')
-xl <- range(p_fpx$embryo.time)
-sapply(seq_along(lins), function(i){
-  l <- lins[[i]]
-  sapply(seq_along(lins), function(j){
-    x <- p_fpx$embryo.time[l]
-    y <- ae_s[[j]]$age.estimates[l,1]
-    
-    plot(x, y, xlim = xl, ylim = xl,
-         main = paste0(names(lins)[i], ' lin /', names(lins)[j], ' ref'), 
-         col = i, xlab = "Blast. time", ylab="Estimated age")
-    
-    abline(a=0, b=1, lty=2) # add x=y
-    # add cor.scores
-    mtext(text = paste0("r = ", round(cor(x, y, method = 'pearson'), 3),
-                        "\nrho =", round(cor(x, y, method = 'spearman'), 3)),
-          at = 1, side = 3, line = -2, cex = .8, adj = 0)
-  })
-})
-dev.off()
-
-# Exploring lineage-inference from staging
-
-svg("fig/neurons_lineage_inference.svg")
-par(mfrow = c(2,3), mar=c(3,2,2,1), bty='l')
-sapply(ctypes[c(1,5,4,6,3,2)], function(ci){
-  ss <- which(cell_subtypes==ci) # select a cell type
+prepare_and_interpolate <- function(X, p, lins, dim_red="ica", nc=8){
+  mt <- multige_im(X = X, p = p, lineages = lins,
+                   dim_red = dim_red, nc = nc,
+                   formulas = c("X~s(embryo.time, bs='cr', k=6)",
+                                "X~s(embryo.time, bs='cr', k=6)",
+                                "X~s(embryo.time, bs='cs', k=6)",
+                                "X~s(embryo.time, bs='cr', k=5)",
+                                "X~s(embryo.time, bs='cr', k=5)",
+                                "X~s(embryo.time, bs='cr', k=5)",
+                                "X~s(embryo.time, bs='cr', k=4)",
+                                "X~s(embryo.time, bs='cr', k=4)"))
   
-  # get cor. score at estimate for each reference
-  ccs <- do.call(cbind, lapply(ae_s, function(a) a$age.estimates[ss,"cor.score"]))
-  colnames(ccs) <- names(lins)
-  boxplot(ccs, border=seq_along(lins), las=2,
-          lwd=2, ylim=c(0, 1), col = 0, boxwex=.4,
-          ylab = "Cor. at estimate", xlab = "reference used", main = ci)
-  mtext(paste0('n=',length(ss)), at=.5, line=-1, cex=.75, side=3, adj = 0)
+  # predict new data (in comp. space)
+  ndat <- data.frame(embryo.time=seq(min(p$embryo.time),
+                                     max(p$embryo.time), l=100))
+  nX <- predict_mgeim(mt, ndat, as.c = T)
+  interpGE <- predict_mgeim(mt, ndat)
   
-})
-dev.off()
+  return(list(mt=mt, ndat=ndat, nX=nX, interpGE=interpGE))
+}
 
-# function to normalize by min value in series
-levelmin <- function(x){(x-min(x))}
+multige <- prepare_and_interpolate(pX, p_fpx, lins)
 
-svg("fig/neurons_lineage_inference_norm.svg")
-par(mfrow = c(2,3), mar=c(3,2,2,1), bty='l', pty='s')
-sapply(ctypes[c(1,5,4,6,3,2)], function(ci){
-  ss <- which(cell_subtypes==ci) # select a cell type
+plot_lineages(multige$mt, multige$ndat, multige$nX,
+              p_fpx$embryo.time, p_fpx$cell.subtype, 1:8)
+
+res <- lapply(1:4, function(i){
+  set.seed(654)
+  p <- 0.8
+  idx <- stratified_split(cell_subtypes, p)
   
-  # get cor. score at estimate for each reference
-  ccs <- do.call(cbind, lapply(ae_s, function(a) a$age.estimates[ss,"cor.score"]))
-  ccs <- t(apply(ccs,1, levelmin))
-  x <- p_fpx$embryo.time[ss]
-  colnames(ccs) <- names(lins)
-  plot(range(p_fpx$embryo.time), range(ccs), type = 'n',
-       lwd=2, ylim=c(0, .05), col = 0,
-       ylab = "Correlation diff. between lineages", xlab = "Blast. time", main = ci)
-  sapply(seq_along(lins), function(i){
-    points(x, ccs[,i], col = transp(i, a = .9), lwd=2)
-  })
-  if(ci==ctypes[1])
-    legend('right', bty='n', legend=names(lins), 
-           lwd=3, lty=NA, pch=1, col=seq_along(lins),
-           title = "Reference")
+  pX_train <- pX[, idx]
+  pX_test <- pX[, -idx]
+  
+  p_fpx_train <- p_fpx[idx, ]
+  p_fpx_test <- p_fpx[-idx, ]
+  
+  lins_train <- lapply(lins,'[', idx)
+  lins_test <- lapply(lins,'[', -idx)
+  
+  multige <- prepare_and_interpolate(pX_train, p_fpx_train, lins_train)
+  
+  ae_s_train <- ale(samp = pX_train, multiref=list(interpGE=multige$interpGE, time=multige$ndat$embryo.time))
+  
+  ae_s_test <- ale(samp = pX_test, multiref=list(interpGE=multige$interpGE, time=multige$ndat$embryo.time))
+  
+  return(list(multige=multige,
+              train=list(idx=idx,
+                         ae_s=ae_s_train),
+              test=list(idx=seq_along(cell_subtypes)[!seq_along(cell_subtypes) %in% idx],
+                        ae_s=ae_s_test)))
 })
-dev.off()
+
+saveRDS(res, file = "./data/neurons_raptor_res.rds")
+rse <- readRDS(file = "./data/neurons_raptor_res.rds")
